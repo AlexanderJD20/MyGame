f0863a8328c83122a7e729cae8ae2cf97edd5023
Bas Pijls <bpijls@gmail.com>
2018-04-05 12:36:53 +0200

    Step 0: Initial Commit

---
+++ ImpHunter/ImpHunter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ImpHunter
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class ImpHunter : GameEnvironment
+    {
+        public ImpHunter()
+        {
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            Screen = new Point(800, 600);
+            ApplyResolutionSettings();
+
+            // TODO: use this.Content to load your game content here
+
+        }
+        
+    }
+}
