56299901e4fb25b41523bbddca656e8b5ebd09d2
AlexanderJD20 <alexdiependaal@gmail.com>
2020-10-27 20:41:10 +0100

    Movement van speler aangepast

--- ImpHunter/GameObjects/Player.cs
+++ ImpHunter/GameObjects/Player.cs
@@ -9,17 +9,38 @@ using Microsoft.Xna.Framework.Input;
 namespace ImpHunter.GameObjects {
     class Player : SpriteGameObject {
         public Player() : base("spr_speler"){
-            Mouse.SetPosition(260, 390);
+            position.X = GameEnvironment.Screen.X/2;
+            position.Y = GameEnvironment.Screen.Y/2;
             origin = Center;
         }
         public override void HandleInput(InputHelper inputHelper) {
             base.HandleInput(inputHelper);
-            this.position = inputHelper.MousePosition;
+            if (inputHelper.KeyPressed(Keys.Right)) {
+                position.X += 20; 
+            }
+            if (inputHelper.KeyPressed(Keys.Left)) {
+                position.X -= 20;
+            }
+            if (inputHelper.KeyPressed(Keys.Up)) {
+                position.Y -= 20;
+            }
+            if (inputHelper.KeyPressed(Keys.Down)) {
+                position.Y += 20;
+            }
         }
         public override void Update(GameTime gameTime) {
             base.Update(gameTime);
-            if(position.X > GameEnvironment.Screen.X) {
-                position.X = GameEnvironment.Screen.X;
+            if(position.X > 650) {
+                position.X = 640;
+            }
+            if (position.X < 150) {
+                position.X = 160;
+            }
+            if (position.Y < 300) {
+                position.Y = 300;
+            }
+            if(position.Y > 800) {
+                position.Y = 800;
             }
         }
 
